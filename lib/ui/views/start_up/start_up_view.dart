e3c68fb12e91a18e636e4862879aea567e3f1e9b
Hussain <hussainbh00@gmail.com>
2021-08-05 01:33:12 +0300

    added a comma

--- lib/ui/views/start_up/start_up_view.dart
+++ lib/ui/views/start_up/start_up_view.dart
@@ -8,8 +8,11 @@ class StartUpView extends StatelessWidget {
     return ViewModelBuilder<StartUpViewModel>.reactive(
       viewModelBuilder: () => StartUpViewModel(),
       onModelReady: (StartUpViewModel viewModel) => viewModel.init(context),
-      builder:
-          (BuildContext context, StartUpViewModel viewModel, Widget? child) {
+      builder: (
+        BuildContext context,
+        StartUpViewModel viewModel,
+        Widget? child,
+      ) {
         return const Scaffold();
       },
     );
