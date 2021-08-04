477683d4396c0c350c43c707bcd22d37d80633ac
Mohamed Mukhtar <mukxdev@gmail.com>
2021-08-05 00:44:48 +0300

    Add font textStyle

---
+++ lib/app/theme/app_font.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+TextStyle fontHeading1 = TextStyle(fontSize: 24, fontWeight: FontWeight.w800);
+
+TextStyle fontHeading2 = TextStyle(fontSize: 18, fontWeight: FontWeight.w700);
+
+TextStyle fontTitle1 = TextStyle(fontSize: 24, fontWeight: FontWeight.w700);
+
+TextStyle fontTitle2 = TextStyle(fontSize: 14, fontWeight: FontWeight.w600);
+
+TextStyle fontParagraph = TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
