531dfb581e48e49fad25851ab5d558af7adfe8cd
Hussain <hussainbh00@gmail.com>
2021-08-05 00:44:23 +0300

    add fonts

--- lib/ui/views/start_up/start_up_view.dart
+++ lib/ui/views/start_up/start_up_view.dart
@@ -1,3 +1,4 @@
+import 'package:fantasia/app/services/size_config.dart';
 import 'package:fantasia/ui/views/start_up/start_up_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
@@ -7,19 +8,40 @@ class StartUpView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<StartUpViewModel>.reactive(
       viewModelBuilder: () => StartUpViewModel(),
-      onModelReady: (StartUpViewModel model) async {
-        await model.init();
-      },
+      onModelReady: (StartUpViewModel viewModel) => viewModel.init(context),
       builder: (
         BuildContext context,
-        StartUpViewModel model,
+        StartUpViewModel viewModel,
         Widget? child,
       ) {
-        return const Scaffold(
-          body: Center(
-            child: Text(
-              'StartUpView',
-            ),
+        return Scaffold(
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                bottom: PreferredSize(
+                  preferredSize: Size(
+                    SizeConfig.screenWidth,
+                    SizeConfig.screenHeight * 3 / 100,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.devicePixelRatio * 23,
+                        bottom: SizeConfig.devicePixelRatio * 10,
+                      ),
+                      child: Text(
+                        'data',
+                        textAlign: TextAlign.start,
+                        textScaleFactor: SizeConfig.devicePixelRatio * 1.5,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
